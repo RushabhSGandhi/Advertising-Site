caf66be7d63de9fb881a8719aba59668f044b8a4
RushabhSGandhi <98331280+RushabhSGandhi@users.noreply.github.com>
2022-01-24 22:26:58 +0530

    Add files via upload

---
+++ app/admin/adds.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Add do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :is_published, :images
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :is_published, :images]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/views/adds/_add.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! add, :id, :title, :description, :is_published, :images, :created_at, :updated_at
+json.url add_url(add, format: :json)
